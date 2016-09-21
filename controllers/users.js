57cbfb22a987d7249ec075c46be6eb15246b587e
nathan oconnor <nathan.oconnor@skybettingandgaming.com>
2016-09-21 17:24:39 +0100

    connected Users controller to Users DB , also fixed package.json to point to app.js

--- controllers/users.js
+++ controllers/users.js
@@ -20,47 +20,91 @@
 //   ownedGames: [3],
 //   image: "https://upload.wikimedia.org/wikipedia/commons/1/14/Rodney_boat.jpg"
 // }];
-var Users = require('../models/mongoose').Users();
+var Users = require('../models/mongoose').Users;
 
 // INDEX
 function indexUser(req, res) {
-  res.render("users/main", {title: "Users", users: users});
+    Users.find({} , function(err, Users) {
+    // check for errors and return 500 error and message if found
+    if(err) return res.status(500).send(err);
+    res.render("Users/main", {title: "Users", Users: Users});
+    });
 }
 
 // SHOW
 function showUser(req, res) {
   //res.send("SHOW:" + req.params.id);
-  var user = users[req.params.id];
-  res.render("users/show", {title: "Game", user: user});
+  Users.findById(req.params.id , function(err, User) {
+  res.render("Users/show", {title: "User", User: User});
+  });
 }
 
 // CREATE
 function createUser(req, res) {
   //get body data (use body parser)
   //posts.push({id : posts.length, title : req.body.title, body : req.body.body})
-  //res.render("posts/show", {title: "User", user: user});
-  res.send("CREATE");
+  //res.render("posts/show", {title: "User", User: User});
+    Users.create( req.body , function(err, post){
+  
+    // check for errors and return 500 if there was a problem
+    if(err) return res.status(500).send(err);
+  
+    // redirect the user to a GET route. We'll go back to the INDEX.
+   res.redirect("/");
+  
+  });
 }
 
 // NEW
 function newUser(req, res) {
-  res.render("users/new", {title: "Create new"});
+    var newUser = {
+      title: "",
+      tags: [],
+      body: "",
+      image: "",
+      comments: []
+  }
+
+  res.render("Users/new" , {
+    title: "New User",
+    User: newUser
+  });
 }
 
 // UPDATE
 function updateUser(req, res) {
-  res.send("UPDATE:" + req.params.id);
+      Users.findByIdAndUpdate( 
+        req.params.id, 
+        { $set:  req.body }, 
+        { runValidators: true }, 
+        function(err , post){
+      
+          if(err) return res.status(500).send(err);
+
+          // redirect the user to a GET route. We'll go back to the INDEX.
+          res.redirect("/");
+    
+        }
+    );
 }
 
 // DELETE
 function deleteUser(req, res) {
-  res.send("DELETE:" + req.params.id);
+    Users.findByIdAndRemove(req.params.id , function(err) {
+      // redirect to a GET request
+      res.redirect("/");
+  });
 }
 
 // EDIT
 function editUser(req, res) {
-  var user = users[req.params.id];
-  res.render("users/edit", {title: "Edit", game: game});
+      Users.findById(req.params.id , function(err, User) {
+    
+      // check for errors or for no object found
+      if(!User) return res.status(404).send("Not found");
+     if(err) return res.status(500).send(err);
+  res.render("Users/edit", {title: "Edit", User: User});
+      });
 }
 
 module.exports = {
